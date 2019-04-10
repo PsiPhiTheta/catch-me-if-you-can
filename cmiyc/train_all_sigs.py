3251e287f6ac07df97c3932d8615df37d8bc8b36
Casey Li <casey@mail.cs.toronto.edu>
2019-04-10 10:15:43 -0400

    kickoff file for training all sigs

---
+++ cmiyc/train_all_sigs.py
@@ -0,0 +1,21 @@
+'''
+Run this to train and save weights on all signatures.
+'''
+import os
+import time
+
+from keras.models import Model
+from keras.layers import Input, Dense, Lambda
+from keras.losses import binary_crossentropy, mse
+from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
+import keras.backend as K
+import numpy as np
+
+import dataset_utils
+import viz_utils
+
+from vanilla_vae import VanillaVae, train_all_sigs
+
+if __name__ == '__main__':
+	train_all_sigs(sig_type='genuine', epochs=3)
